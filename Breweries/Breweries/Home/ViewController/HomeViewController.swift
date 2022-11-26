a601608fb55f06515af60b7d07ab727a2dad682e
Ramon Queiroz dos Santos <ramonqueirozs@gmail.com>
2022-11-26 15:49:11 -0300

    Feat: Criado estilização NavBar

--- Breweries/Breweries/Home/ViewController/HomeViewController.swift
+++ Breweries/Breweries/Home/ViewController/HomeViewController.swift
@@ -18,9 +18,42 @@ class HomeViewController: UIViewController {
 		self.view = screen
 	}
 	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-	}
-	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
 
+extension HomeViewController {
+	private func setupNavigationBar() {
+		navigationController?.navigationBar.backgroundColor = UIColor.yellowDark()
+		navigationController?.navigationBar.isTranslucent = false
+		setupNavigationBarItems()
+	}
+	
+	private func setupNavigationBarItems() {
+		navigationItem.title = "Breweries"
+		setupLeftNavigationBar()
+		setupRightNavigationBar()
+	}
+	
+	private func setupLeftNavigationBar() {
+		let logoIcon = UIButton(type: .system)
+		logoIcon.setImage(UIImage(named: "icon"), for: .normal)
+		logoIcon.tintColor = .black
+		navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoIcon)
+	}
+	
+	private func setupRightNavigationBar() {
+		let favoriteIcon = UIButton(type: .system)
+		favoriteIcon.setImage(UIImage(named: "favorite_border")?.withRenderingMode(.alwaysOriginal), for: .normal)
+		
+		let starIcon = UIButton(type: .system)
+		starIcon.setImage(UIImage(named: "star_border")?.withRenderingMode(.alwaysOriginal), for: .normal)
+		starIcon.frame = CGRect(x: 0, y: 0, width: 40, height: 30)
+		
+		navigationItem.rightBarButtonItems = [
+			 UIBarButtonItem(customView: favoriteIcon),
+			 UIBarButtonItem(customView: starIcon)
+		]
+	}
+}
